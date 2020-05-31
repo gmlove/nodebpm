a5d6af08e1608350160e01ad8ddce686f5624564
gmlove <gracekinglau@gmail.com>
2020-05-31 12:48:50 +0800

    refactor code and add jquery dependency

--- public/bp.js
+++ public/bp.js
@@ -1,50 +1,44 @@
-var codeEditor = CodeMirror.fromTextArea(document.getElementById("data"), {
-    matchBrackets: true,
-    autoCloseBrackets: true,
-    matchTags: {bothTags: true},
-    autoCloseTags: true,
-    foldGutter: true,
-    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-    lint: true,
-    mode: "application/ld+json",
-    lineNumbers: true,
-    lineWrapping: true,
-    spellcheck: true,
-    indentUnit: 4,
-    theme: 'base16-light'
-});
-codeEditor.setSize(null, 150);
+(function (_, $) {
 
-var contentEditor = CodeMirror.fromTextArea(document.getElementById("content"), {
-    mode: "text/html",
-    matchBrackets: true,
-    autoCloseBrackets: true,
-    matchTags: {bothTags: true},
-    autoCloseTags: true,
-    foldGutter: true,
-    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-    lint: true,
-    lineNumbers: true,
-    spellcheck: true,
-    indentUnit: 4,
-    theme: 'base16-light'
-});
-contentEditor.setSize(null, 450);
+    var commonOpts = {
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        matchTags: { bothTags: true },
+        autoCloseTags: true,
+        foldGutter: true,
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+        lint: true,
+        lineNumbers: true,
+        lineWrapping: true,
+        spellcheck: true,
+        indentUnit: 4,
+        theme: 'base16-light'
+    };
 
-var scriptEditor = CodeMirror.fromTextArea(document.getElementById("script"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    autoCloseBrackets: true,
-    matchTags: {bothTags: true},
-    autoCloseTags: true,
-    foldGutter: true,
-    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-    lint: true,
-    spellcheck: true,
-    continueComments: "Enter",
-    extraKeys: {"Ctrl-Q": "toggleComment"},
-    indentUnit: 4,
-    theme: 'base16-light'
-});
-scriptEditor.setSize(null, 450);
+    var codeEditor = CodeMirror.fromTextArea(
+        document.getElementById("data"),
+        _.merge(commonOpts, { mode: "application/ld+json" })
+    );
+    codeEditor.setSize(null, 150);
 
+    var contentEditor = CodeMirror.fromTextArea(
+        document.getElementById("content"),
+        _.merge(commonOpts, { mode: "text/html" })
+    );
+    contentEditor.setSize(null, 450);
+
+    var scriptEditor = CodeMirror.fromTextArea(
+        document.getElementById("script"),
+        _.merge(commonOpts, { mode: "javascript" })
+    );
+    scriptEditor.setSize(null, 450);
+
+
+    $.ajax({
+        url: "bp.html",
+        context: document.body
+    }).done(function () {
+        console.log('done');
+    });
+
+})(_, $)
